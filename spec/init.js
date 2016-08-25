0a59056d1d3ac14b94605e33a00370bf2bf84064
Timothy Shiu <timoweave@gmail.com>
2016-08-25 13:16:10 -0700

    test(runner): add mocha, travis-ci

---
+++ spec/init.js
@@ -0,0 +1,8 @@
+var assert = require("chai").assert;
+var expect = require("chai").expect;
+
+describe("sample", function() {
+  it ("should true be true", function() {
+    expect(true).to.equal(true);
+  });
+});
