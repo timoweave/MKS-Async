f8be659a9996a14ae65e78f1c5432f217a8bdf85
Timothy Shiu <timoweave@gmail.com>
2016-08-25 20:27:55 -0700

    chore(db): add booking and comment mongo document and express restful

--- server/config.js
+++ server/config.js
@@ -1,14 +1,13 @@
 var config = {
-    server : {
-      port : 3000
-    },
-    mongoose: {
-      local : 'mongodb://localhost:27017/async',
-      mlab : 'mongodb://asyncuser:asyncpass@ds053190.mlab.com:53190/async',
-      option : {
-        server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
-        replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } }
-      }
+  server: {
+    port: 3000
+  },
+  mongoose: {
+    local: 'mongodb://localhost:27017/async',
+    mlab: 'mongodb://asyncuser:asyncpass@ds053190.mlab.com:53190/async',
+    option: {
+      server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+      replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
     }
   }
 };
