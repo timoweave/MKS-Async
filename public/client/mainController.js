c148b2bd72c701180ea395c1b4eb6d6c3ccf9685
Cindy Sun <c.s.sun5@gmail.com>
2016-08-29 22:22:26 -0700

    refactor(web-client): use directives for ads list

--- public/client/mainController.js
+++ public/client/mainController.js
@@ -1,13 +1,13 @@
 angular.module('async.mainController', ['ui.bootstrap'])
 
 .controller('MainController', ['$scope', '$uibModal', function($scope, $uibModal) {
+
   $scope.openForm = function() {
     $uibModal.open({
       templateUrl: 'client/postForm/postForm.html',
       controller: 'FormController'
     });
   };
-
 }])
 
 .controller('AdsController', ['$scope', '$uibModal', 'Ads', function($scope, $uibModal, Ads) {
@@ -23,5 +23,15 @@ angular.module('async.mainController', ['ui.bootstrap'])
         console.eror(err);
       });
   };
+}])
 
-}]);
+.directive('providerAds', function() {
+  return {
+    scope: {
+      item: '=providerAds'
+    },
+    restrict: 'EA',
+    templateUrl: 'client/adsList/adsList.html'
+  };
+});
+
