26509b995b1d85eeeada9f560ce6e8add3cbb017
Sergey Potashnikov <sdp781@gmail.com>
2016-08-30 11:27:13 -0700

    chore(runner): add Grunt TaskRunner

---
+++ Gruntfile.js
@@ -0,0 +1,136 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      dist: {
+        src: [
+          'public/client/**/*.js',
+        ],
+        dest: 'public/dist/production.js',
+      }
+    },
+
+    // mochaTest: {
+    //   test: {
+    //     options: {
+    //       reporter: 'spec'
+    //     },
+    //     src: ['test/**/*.js']
+    //   }
+    // },
+
+    nodemon: {
+      dev: {
+        script: 'server/server.js'
+      }
+    },
+
+    uglify: {
+      build: {
+        src: 'public/dist/production.js',
+        dest: 'public/dist/production.min.js'
+      }
+    },
+
+    // jshint: {
+    //   files: [
+    //     'Gruntfile.js', 'public/client/*.js'
+    //   ],
+    //   options: {
+    //     force: 'true',
+    //     jshintrc: '.jshintrc',
+    //     ignores: [
+    //       'public/lib/**/*.js',
+    //       'public/dist/**/*.js'
+    //     ]
+    //   }
+    // },
+
+    cssmin: {
+      dist:{
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'public/dist/style.css': 'public/client/styles/style.css'
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: [
+          'public/client/**/*.js',
+          'public/lib/**/*.js',
+        ],
+        tasks: [
+          'concat',
+          'uglify'
+        ]
+      },
+      css: {
+        files: 'public/client/styles/*.css',
+        tasks: ['cssmin']
+      }
+    },
+
+    shell: {
+      prodServer: {
+          command: 'git push heroku master'
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('server-dev', function (target) {
+    // Running nodejs in a different process and displaying output on the main console
+    var nodemon = grunt.util.spawn({
+         cmd: 'grunt',
+         grunt: true,
+         args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
+
+    // grunt.task.run([ 'watch' ]);
+  });
+
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('test', [
+    'mochaTest'
+  ]);
+
+  grunt.registerTask('build', [
+    // 'test', <-- uncomment once tests are written/ready
+    'concat', 'uglify', 'cssmin'
+  ]);
+
+  // grunt.registerTask('watch', ['watch']);
+
+  grunt.registerTask('upload', function(n) {
+    if(grunt.option('prod')) {
+      // add your production server task here
+        grunt.task.run([ 'shell:prodServer' ]);
+    } else {
+      grunt.task.run([ 'server-dev' ]);
+    }
+  });
+
+  grunt.registerTask('deploy', [
+    'build', 'upload'
+  ]);
+
+};
--- server/server.js
+++ server/server.js
@@ -11,8 +11,12 @@ var app = express();
 app.set('port', (process.env.PORT) || 3000);
 app.use(morgan('combined'));
 
+<<<<<<< 6c06e2d921d19844c6210acc4de02facbfd36833
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname,'../bower_components')));
+=======
+
+>>>>>>> Grunt near completion, committing in order to rebase
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -22,7 +26,11 @@ app.use('/api', models.Router);
 app.get('/', function(req, res) {
   // var something = req.body.something;
   //do something with 'something'
+<<<<<<< 6c06e2d921d19844c6210acc4de02facbfd36833
   res.sendFile(path.join(__dirname, '../public/index.html'));
+=======
+
+>>>>>>> Grunt near completion, committing in order to rebase
 });
 
 app.post('/', function(req, res) {
@@ -34,5 +42,8 @@ app.post('/', function(req, res) {
 
 var server = app.listen(app.get('port'), function() {
   console.log(chalk.green('OK'), 'server', server.address().port);
+<<<<<<< 6c06e2d921d19844c6210acc4de02facbfd36833
+=======
+>>>>>>> Grunt near completion, committing in order to rebase
 });
 
