d61c232dbfbab186144293d15f2451d6ab24facf
Timothy Shiu <timoweave@gmail.com>
2016-08-31 15:35:25 -0700

    test(db): add testing

---
+++ spec/models.js
@@ -0,0 +1,56 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe("database model", function() {
+
+  beforeEach(function() {
+    
+  });
+
+  afterEach(function() {
+    
+  });
+
+  it("user", function() {
+    expect(true).to.equal(true);
+  });
+
+  it("post", function() {
+    expect(true).to.equal(true);
+  });
+
+  it("comment", function() {
+    expect(true).to.equal(true);
+  });
+
+  it("booking", function() {
+    expect(true).to.equal(true);
+  });
+});
+
+describe("restful api", function() {
+
+  beforeEach(function() {
+    
+  });
+
+  afterEach(function() {
+    
+  });
+
+  it("user", function() {
+    expect(true).to.equal(true);
+  });
+
+  it("post", function() {
+    expect(true).to.equal(true);
+  });
+
+  it("comment", function() {
+    expect(true).to.equal(true);
+  });
+
+  it("booking", function() {
+    expect(true).to.equal(true);
+  });
+});
