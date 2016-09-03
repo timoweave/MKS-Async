82b683f735461de4b0935109fd8e30c8dccdb325
Cindy Sun <c.s.sun5@gmail.com>
2016-09-02 22:43:27 -0700

    fix(web-client): show all ads in providerDash route

--- public/client/providerDash/providerController.js
+++ public/client/providerDash/providerController.js
@@ -1,10 +1,22 @@
 angular.module('async.providerController', ['ui.bootstrap'])
 
-.controller('ProviderController', ['$scope', '$uibModal', function($scope, $uibModal){
+.controller('ProviderController', ['$scope', '$uibModal', 'Ads', function($scope, $uibModal, Ads){
    $scope.openForm = function() {
       $uibModal.open({
         templateUrl: 'client/postForm/postForm.html',
         controller: 'FormController'
       });
+    };
+
+    $scope.ads={};
+
+    $scope.getAds = function(){
+      Ads.getAds()
+        .then(function(data) {
+          $scope.ads = data;
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
     };
 }]);
